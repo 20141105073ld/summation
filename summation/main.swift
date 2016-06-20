d1a134fe227a1f215b4386671613ad5693621e55
20141105073 <a20141105073@admindeimac-12.local>
2016-06-20 11:04:53 +0800

    Initial Commit

---
+++ summation/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  summation
+//
+//  Created by 20141105073 on 16/6/20.
+//  Copyright © 2016年 20141105073. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
